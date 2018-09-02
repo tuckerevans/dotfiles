0bb0e49021e267713986240333d0cffe936f8dac
Tucker Evans <tuckerevans24@gmail.com>
2018-09-02 15:53:41 -0400

    Adds minor changes to devon configs

--- X/.screenSetup.sh
+++ X/.screenSetup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-1 --primary --mode 1920x1080 --pos 0x840 --rotate normal --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate right
+xrandr --output DVI-I-1 --primary --mode 1920x1080 --pos 0x840 --rotate normal --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate right
