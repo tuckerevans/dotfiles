1cdf585d43ef44f721daaee44b7a5f266775289f
Tucker Evans <tucker@tuckerevans.com>
2020-09-17 13:34:05 -0400

    Update lemonbar scripts

---
+++ lemonbar/bin/get_network_info.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+for DEV in `ip -o addr | awk '{if($2 != "lo" && $3 == "inet"){print $2}}'`
+do
+	SSID=""
+	OUTPUT=""
+	ADDR=`ip addr show dev "$DEV" | grep -E "inet[^6]" \
+	| awk '{print $2}'`
+	MATCH=`expr "$DEV" : "^wlp"`
+	if test "$MATCH" -gt 0
+	then
+		SSID=`iw dev "$DEV" link | grep SSID | awk '{print $2}'`
+		SSID="$SSID: "
+	fi
+
+	if test -n "$OUTPUT"
+	then
+		OUTPUT="$OUTPUT | ($DEV) $SSID$ADDR"
+	else
+		OUTPUT="($DEV) $SSID$ADDR"
+	fi
+done
+
+echo "$OUTPUT"
